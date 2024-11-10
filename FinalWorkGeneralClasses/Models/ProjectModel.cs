bc373eca0f824290b0bcdc32a83e7a13b71cda83
konstantin makhnutin <mahnutin@mail.ru>
2024-11-10 20:05:43 +0300

    ProjectModel added

---
+++ FinalWorkGeneralClasses/Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalWorkGeneralClasses.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+
+        public string ProjectTitle { get; set; }
+
+        public string ProjectDescr { get; set; }
+
+        public string ProjectPhotoLink { get; set; }
+    }
+}
