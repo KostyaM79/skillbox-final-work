11ecf7cce45670f2fc7528914598d64a2af38e4d
konstantin makhnutin <mahnutin@mail.ru>
2024-11-10 19:43:21 +0300

    addition project done

--- WebClient/wwwroot/js/site.js
+++ WebClient/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-const { start } = require("@popperjs/core");
+const { read } = require("@popperjs/core");
 
 function setDateRange() {
     let dateElement1 = document.getElementById("date1");
@@ -22,4 +22,29 @@ function diffDate(d1, d2) {
     let t1 = d1.getTime();
     let t2 = d2.getTime();
     return Math.floor((t1 - t2) / (24 * 3600 * 1000));
+}
+
+function show_picture() {
+    let element = document.getElementById("proj-photo");        // Получаем input-элемент
+    let f = element.files[0];                                   // Получаем ссылку на выбранный файл
+
+    var img = document.getElementById("proj-img");
+
+    if (!img) {
+        img = document.createElement("img");
+        img.classList.add("add-project-frm_img");
+        img.id = "proj-img";
+        document.getElementById("frm-image").appendChild(img);
+    }
+
+    img.file = f;
+
+    var reader = new FileReader();
+    reader.onload = (function (aImg) {
+        return function (e) {
+            aImg.src = e.target.result;
+        }
+    })(img);
+
+    reader.readAsDataURL(f);
 }
