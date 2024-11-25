c4b50625021993e8edafbb5fb00a1f1940798718
konstantin makhnutin <mahnutin@mail.ru>
2024-11-26 01:34:18 +0300

    socials form added

--- WebClient/wwwroot/js/site.js
+++ WebClient/wwwroot/js/site.js
@@ -94,3 +94,82 @@ function showFrm() {
     let frm = document.getElementById("order-frm");
     frm.style.display = "flex";
 }
+
+// Отображает модальное окно для редактирования соц. сетей
+function show_modal() {
+    let modal = document.getElementById("edit-contacts");
+    modal.style.visibility = "visible";
+    let basket = modal.getElementsByClassName("modal__basket")[0];
+    basket.onclick = remove_item;
+    let file = modal.getElementsByClassName("modal-file")[0];
+    file.onchange = fileOnChanged;
+}
+
+//Добавляет элемент в список соц. сетей
+function add_item() {
+    let modal = document.getElementById("edit-contacts"); // Получаем экземпляр модального окна
+
+    let inputs = modal.getElementsByClassName("modal__input");
+    if (inputs[inputs.length - 1].value) {
+
+        let item = document.createElement("li");
+        let input = document.createElement("input");
+        let img = document.createElement("img");
+        let basket = document.createElement("img");
+        let label = document.createElement("label");
+        let inputFile = document.createElement("input");
+        let inputHide = document.createElement("input");
+
+        inputHide.setAttribute("type", "hidden");
+
+        inputFile.setAttribute("type", "file");
+        inputFile.classList.add("modal-file");
+        inputFile.onchange = fileOnChanged;
+
+        label.appendChild(inputFile);
+        label.appendChild(img);
+
+        item.classList.add("modal__item");
+
+        input.setAttribute("type", "text");
+        input.classList.add("modal__input");
+
+        img.style.width = "24px";
+        img.style.height = "24px";
+        img.src = "../img/add-icon.svg";
+        img.classList.add("modal__add");
+
+        basket.style.width = "24px";
+        basket.style.height = "24px";
+        basket.src = "../img/delete.svg";
+        basket.classList.add("modal__basket");
+        basket.onclick = remove_item;
+
+        item.append(inputHide);
+        item.appendChild(label);
+        item.appendChild(input);
+        item.appendChild(basket);
+
+        let items = modal.getElementsByClassName("modal__links")[0];
+        items.appendChild(item);
+    }
+}
+
+// Удаляет ссылку из модального окна
+function remove_item(e) {
+    let modal = document.getElementById("edit-contacts");           // Получаем экземпляр модального окна
+    let items = modal.getElementsByClassName("modal__links")[0];    // Получаем список ссылок
+    let basket = e.currentTarget;
+    let removing = basket.parentNode;                               // Получаем item
+    if (items.childElementCount > 1) {
+        items.removeChild(removing);                                // Удаляем item
+    }
+}
+
+function fileOnChanged(e) {
+    let file = e.currentTarget;
+    let item = file.parentNode;
+    let img = item.getElementsByClassName("modal__add")[0];
+    let path = file.files[0];
+    img.src = URL.createObjectURL(path);
+}
